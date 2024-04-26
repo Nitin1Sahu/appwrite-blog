3621dd00ae0cde679d23e75ac9d3dfd6f21c3797
Nitin Sahu <sahunitin970@gmail.com>
2024-04-26 15:41:08 +0530

    add appwrite authentication and storage functionality

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { config } from './config'
+
+function App() {
+  console.log(config.appwriteUrl)
+  return (
+    <>
+      <h1>Blog with appwrite</h1>
+    </>
+  )
+}
+
+export default App
