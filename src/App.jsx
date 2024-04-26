0b2625ebb73616ee784732ecd1355259c89d0ecb
Nitin Sahu <sahunitin970@gmail.com>
2024-04-26 16:55:46 +0530

    setup react redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth.js'
+import { login, logout } from './store/authSlice.js'
+import {Header, Footer} from './components'
 import './App.css'
-import { config } from './config'
 
 function App() {
-  console.log(config.appwriteUrl)
-  return (
-    <>
-      <h1>Blog with appwrite</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
+  useEffect(() => {
+    authService.getGetCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO: {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (null)
 }
 
 export default App
